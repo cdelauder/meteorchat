3ab16cf543311e6ea96cc274e2ad5adaf4596915
cdelauder <cdelauder@gmail.com>
2014-07-31 15:24:05 -0700

    simple chat app built! today I am a winner

--- meteorchat/client/client.js
+++ meteorchat/client/client.js
@@ -5,7 +5,10 @@ Template.messages.messages = function() {
 Template.input.events = {
   'keydown input#message': function(event) {
     if (event.which == 13) {
-      var name = 'unknown'
+      if (Meteor.user())
+        var name = Meteor.user().profile.name
+      else
+        var name = 'unknown'
       var message = document.getElementById('message').value
 
       if (message != '') {
